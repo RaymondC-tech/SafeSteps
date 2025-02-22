409079719fe856da1ba5c949d2604b54fc653be1
Sahajdeep Singh <impulseVD@hotmail.com>
2025-02-22 00:29:10 -0500

    added way to get base route from google, added config which is gitignored

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
-import random
+from pydantic import BaseModel
+import requests
+
+import config
 
 app = FastAPI()
+
+class PathRequest(BaseModel):   # Class for input parameters using pydantic model that works well with API data types
+    start_lat: float
+    start_lon: float
+    goal_lat: float
+    goal_lon: float
+
+def get_route_from_google(start_lat, start_lng, end_lat, end_lng):
+    API_KEY = config.google_api_key
+    url = f"https://maps.googleapis.com/maps/api/directions/json?origin={start_lat},{start_lng}&destination={end_lat},{end_lng}&mode=walking&key={API_KEY}"
+    response = requests.get(url)
+    return response.json()
 
 @app.get('/')
 async def root():
